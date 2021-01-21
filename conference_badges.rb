a3e56980508d6e395d687bf370f5374ef894d920
Kiana Long <kianajade42.kl@gmail.com>
2021-01-21 18:02:08 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,3 +3,8 @@ attendees=["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 def badge_maker(name)
  "Hello, my name is #{name}."
 end
+def batch_badge_creator(attendees)
+  attendees.each do |attendee|
+   "Hello, my name is #{attendee}."
+end
+
